d55953477a4b4c9f25917487b7d9aff2e9e4161c
Carlos Silva <c.edusilva@yahoo.com>
2023-12-29 10:31:40 -0300

    correção na classe iphone e adicionando os teste no program

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,14 +1,14 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    class Iphone : Smartphone
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
-        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, imei, modelo, memoria)
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
         {
+
         }
 
-
+        // TODO: Sobrescrever o método "InstalarAplicativo"
         public override void InstalarAplicativo(string nomeApp)
         {
             Console.WriteLine($"Aplicativo {nomeApp} instalado com sucesso no Iphone.");
--- Program.cs
+++ Program.cs
@@ -1,3 +1,64 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main()
+    {
+        // Criar uma instância da classe Iphone
+        Console.Write("Digite o número do Iphone: ");
+        string numeroIphone = Console.ReadLine();
+
+        Console.Write("Digite o modelo do Iphone: ");
+        string modeloIphone = Console.ReadLine();
+
+        Console.Write("Digite o IMEI do Iphone: ");
+        string imeiIphone = Console.ReadLine();
+
+        Console.Write("Digite o memoria do Iphone: ");
+        int memoriaIphone = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o APP do Iphone para download: ");
+        string appIphone = Console.ReadLine();
+
+        Iphone iphone = new Iphone(numeroIphone, modeloIphone, imeiIphone, memoriaIphone);
+        Console.WriteLine($"Número Iphone: {iphone.Numero}\nModelo Iphone: {iphone.Modelo}\nImei Iphone: {iphone.Imei}\nMemoria Iphone: {iphone.Memoria}");
+
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+
+        iphone.InstalarAplicativo(appIphone);
+
+
+
+        //Nokia
+        Console.Write("Digite o número da Nokia: ");
+        string numeroNokia = Console.ReadLine();
+
+        Console.Write("Digite o modelo da Nokia: ");
+        string modeloNokia = Console.ReadLine();
+
+        Console.Write("Digite o IMEI da Nokia: ");
+        string imeiNokia = Console.ReadLine();
+
+        Console.Write("Digite o memoria da Nokia: ");
+        int memoriaNokia = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o APP da Nokia para download: ");
+        string appNokia = Console.ReadLine();
+
+
+        // Chamar métodos da classe base
+        Nokia nokiaPhone = new Nokia(numeroNokia, modeloNokia, imeiNokia, memoriaNokia);
+
+
+        nokiaPhone.Ligar();
+        nokiaPhone.ReceberLigacao();
+
+        Console.WriteLine($"Número Nokia: {nokiaPhone.Numero}\nModelo Nokia: {nokiaPhone.Modelo}\n" +
+            $"Imei Nokia: {nokiaPhone.Imei}\nMemoria Nokia: {nokiaPhone.Memoria}");
+        // Testar o método InstalarAplicativo da classe derivada Nokia
+        nokiaPhone.InstalarAplicativo(appNokia);
+
+        Console.ReadLine();
+    }
+}
